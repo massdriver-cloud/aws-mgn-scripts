ff4e7e56f889b6825d68eaee0ad02d5ec0d36ac6
Michael Lacore <michael@massdriver.cloud>
2024-12-12 11:40:22 -0800

    Initial commit of scripts and README (#1)
    * Initial commit of scripts and README
    
    * standardizing placeholder values in csv
    
    * clarifying migration inventory necessity
    
    * moving optional tag
    
    * grammar
    
    * defaulting open expandables
    
    * moving code block
    
    * adding input validation
    
    * moved install scripst to new dir, added generate install script
    
    * updated readme for new script generator
    
    * adding fork repo step
    
    * add prereq block readme
    
    * prettier comment blocks

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # aws-mgn-scripts
+
 Scripts for initializing AWS MGN and installing AWS MGN replication agents
+
+## Prerequisites
+
+1. Configure AWS creds using `aws configure`.
+2. Fork this repo.
+
+## MGN Setup
+
+### Initialize AWS MGN Service
+
+Run `./init.sh` and answer prompts.
+
+### Create Migration Inventory (optional)
+
+> [!TIP]
+>  Migration inventory is great for migration multiple VMs across multiple migration waves, or VMs that are dependent on other VMs (grouped into the same "Application").
+
+1. Run `./inventory.sh` or manually add data to `aws-application-migration-service-import.csv`.
+2. Replace all of the `REPLACE` keys with desired values.
+3. Update all of the indexes (`0`) where needed.
+4. Import completed inventory using `./import.sh`.
+
+## Agent Installation
+
+1. Run `./generate_install_script.sh` and answer prompts.
+2. Copy code output and paste into source server to download and install the replication agent.
+3. Check AWS MGN console for source server replication status or use:
+
+```bash
+aws mgn describe-source-servers --region <region>
+```
---
+++ inventory.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#################################################################
+# Copyright 2024 Massdriver, Inc                                #
+#                                                               #
+# This script checks for (or creates) an inventory CSV file     #
+# for configuring an MGN inventory. This CSV file is useful     #
+# when application servers have multiple dependencies on other  #
+# servers, or when a migration is to be done in multiple waves. #
+#                                                               #
+# To use:                                                       #
+# 1. Replace all of the REPLACE keys with desired values        #
+# 2. Update all of the indexes where needed                     #
+#                                                               #
+#################################################################
+
+CSV_FILE="./aws-application-migration-service-import.csv"
+
+HEADER="mgn:account-id,mgn:region,mgn:wave:name,mgn:wave:tag:REPLACE,mgn:wave:description,mgn:app:name,mgn:app:tag:REPLACE,mgn:app:description,mgn:server:user-provided-id,mgn:server:platform,mgn:server:tag:REPLACE,mgn:server:fqdn-for-action-framework,mgn:launch:nic:0:network-interface-id,mgn:launch:nic:0:subnet-id,mgn:launch:nic:0:security-group-id:0,mgn:launch:nic:0:private-ip:0,mgn:launch:instance-type,mgn:launch:placement:tenancy,mgn:launch:iam-instance-profile:name,mgn:launch:placement:host-id,mgn:launch:volume:REPLACE:type"
+
+
+if [[ ! -f $CSV_FILE ]]; then
+    echo "File does not exist. Creating file and adding headers."
+    echo "$HEADER" > "$CSV_FILE"
+fi
+
+add_row() {
+    local row=()
+    IFS=',' read -ra fields <<< "$HEADER"
+    for field in "${fields[@]}"; do
+        read -p "Enter value for $field: " value
+        row+=("$value")
+    done
+
+    echo "$(IFS=','; echo "${row[*]}")" >> "$CSV_FILE"
+    echo "Row appended successfully to $CSV_FILE"
+}
+
+while true; do
+    echo "Add a new row:"
+    add_row
+    read -p "Do you want to add another row? (yes/no): " add_another
+    if [[ ! "$add_another" =~ ^(yes|y)$ ]]; then
+        echo "Finished adding rows."
+        break
+    fi
+done
